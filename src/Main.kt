6b026dfb22d45a8ca18770900bd3d3036d2507b0
Shepora <alarconchavezluisalejandro@gmail.com>
2025-02-27 19:57:33 -0500

    Prueba 001 en proceso

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    var Mensaje  = "Prueba 001"
+    var Mensaje  = "Prueba 001, se llega hasta aqui vamos bien"
     println(Mensaje)
+
 }
