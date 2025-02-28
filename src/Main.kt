29fbe303bd823e4fcf731fbcc64fb3c649aef895
Shepora <alarconchavezluisalejandro@gmail.com>
2025-02-27 20:26:31 -0500

    Prueba

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     var Mensaje  = "Prueba 001, se llega hasta aqui vamos bien"
     println(Mensaje)
 
-    var Mensaje1  = "Prueba 002, Prueba para el revert"
+    var Mensaje1  = "Prueba 002, Siguiente prueba"
     println(Mensaje1)
 
 }
